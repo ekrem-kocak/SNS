74bf713025348f4a41241969a6f8b1b2e2e6e22a
ekrem.kocak <ekrem-kocak@outlook.com>
2022-12-11 19:16:42 +0300

    auth process

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlertService } from 'src/app/shared/services/alert.service';
+import { StateService } from 'src/app/shared/services/state.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -8,9 +11,26 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ForgotPasswordComponent {
 
+
   forgotPasswordForm: FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
   })
 
+  constructor(private authService: AuthService, private alertService: AlertService, private stateService: StateService) { }
+
   get email() { return this.forgotPasswordForm.get('email') }
+
+  resetPassword() {
+    this.stateService.setLoading(true)
+    this.authService.resetPassword(this.email?.value).subscribe({
+      next: (res) => {
+        this.alertService.success("A password reset email has been sent. Please also check your spam box.");
+      },
+      error: (err) => {
+        this.alertService.error(err)
+        this.stateService.setLoading(false)
+      },
+      complete: () => this.stateService.setLoading(false)
+    })
+  }
 }
